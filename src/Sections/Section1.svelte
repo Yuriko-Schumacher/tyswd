4e511ab97540d4e852018c69f794c5a8c6dc160a
Yuriko Schumacher <yuriko.schumacher@gmail.com>
2021-12-08 23:13:07 -0500

    fix flourish tables

--- src/Sections/Section1.svelte
+++ src/Sections/Section1.svelte
@@ -2,7 +2,6 @@
   import * as d3 from 'd3';
   import Scroller from "@sveltejs/svelte-scroller";
   import { spring } from 'svelte/motion';
-  import { cubicOut } from 'svelte/easing';
 
   export let data;
   let index, offset, progress;
@@ -113,8 +112,27 @@
   </article>
 </section>
 
-<div class="flourish-embed flourish-table" data-src="visualisation/8040745">
-  <script src="https://public.flourish.studio/resources/embed.js"></script>
+<div class="flourish-embed">
+  <iframe
+    src='https://flo.uri.sh/visualisation/8040745/embed'
+    title='Interactive or visual content'
+    class='flourish-embed-iframe'
+    frameborder='0'
+    scrolling='no'
+    style='width:100%;height:500px;'
+    sandbox='allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation'>
+  </iframe>
+  <div style='width:100%!;margin-top:4px!important;text-align:right!important;'>
+    <a
+      class='flourish-credit'
+      href='https://public.flourish.studio/visualisation/8040745/?utm_source=embed&utm_campaign=visualisation/8040745'
+      target='_top'
+      style='text-decoration:none!important'>
+        <img 
+          alt='Made with Flourish'
+          src='https://public.flourish.studio/resources/made_with_flourish.svg'
+          style='width:105px!important;height:16px!important;border:none!important;margin:0!important;'> </a>
+  </div>
 </div>
 
 <Scroller top="{0}" bottom="{1}" bind:index bind:offset bind:progress>
